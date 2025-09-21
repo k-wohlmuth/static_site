d0a14695366bd8ba2ef3c3b296f9fd780ff4cdff
kwohlmuth <kwohlmuth@gmail.com>
2025-09-20 20:03:13 -0500

    correcting issue with url

--- src/gencontent.py
+++ src/gencontent.py
@@ -15,28 +15,24 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, bas
 
 def generate_page(from_path, template_path, dest_path, basepath):
     print(f" * {from_path} {template_path} -> {dest_path}")
-    from_file = open(from_path, "r")
-    markdown_content = from_file.read()
-    from_file.close()
 
-    template_file = open(template_path, "r")
-    template = template_file.read()
-    template_file.close()
+    with open(from_path, "r") as f:
+        markdown_content = f.read()
+    with open(template_path, "r") as f:
+        template = f.read()
 
     node = markdown_to_html_node(markdown_content)
     html = node.to_html()
 
     title = extract_title(markdown_content)
-    template = template.replace("{{ Title }}", title)
-    template = template.replace("{{ Content }}", html)
-    template = template.replace('href="/', 'href="' + basepath)
-    template = template.replace('src="/', 'src="' + basepath)
+    page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    page = page.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
 
-    dest_dir_path = os.path.dirname(dest_path)
-    if dest_dir_path != "":
-        os.makedirs(dest_dir_path, exist_ok=True)
-    to_file = open(dest_path, "w")
-    to_file.write(template)
+    dest_dir = os.path.dirname(dest_path)
+    if dest_dir:
+        os.makedirs(dest_dir, exist_ok=True)
+    with open(dest_path, "w") as f:
+        f.write(page)
 
 
 def extract_title(md):
